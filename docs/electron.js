c875f45b4cfe895f3a975b0bb0fe15eba2cbc4fb
ASD0x41 <ASD0x41@users.noreply.github.com>
2025-02-04 21:05:26 +0500

    Shift deployment to main repository

---
+++ docs/electron.js
@@ -0,0 +1,77 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let isDev;
+
+(async () => {
+  isDev = (await import('electron-is-dev')).default;
+})();
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      nodeIntegration: false, 
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    },
+    minWidth: 800,
+    minHeight: 600,
+    resizable: false,
+  });
+
+  const startUrl = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, 'index.html')}`;
+
+  mainWindow.loadURL(startUrl);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  mainWindow.maximize();
+}
+
+app.on('ready', () => {
+  setTimeout(createWindow, 500);
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('minimize-window', () => {
+  if (mainWindow) {
+    mainWindow.minimize();
+  }
+});
+
+ipcMain.on('maximize-window', () => {
+  if (mainWindow) {
+    if (mainWindow.isMaximized()) {
+      mainWindow.unmaximize();
+    } else {
+      mainWindow.maximize();
+    }
+  }
+});
+
+ipcMain.on('close-window', () => {
+  if (mainWindow) {
+    mainWindow.close();
+  }
+});
